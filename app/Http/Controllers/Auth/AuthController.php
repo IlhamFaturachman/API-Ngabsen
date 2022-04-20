350edaff50883af74317fdd169b15894383b4342
IlhamFaturachman <69384419+IlhamFaturachman@users.noreply.github.com>
2022-04-20 20:38:20 +0700

    anjay

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -26,15 +26,8 @@ class AuthController extends Controller
             ]);
         }
 
-        if ($user->role === 'siswa'){
-            $data_user = User::with('student')->where('id_number', $input['id_number'])->where('password', $input['password'])->first();
-        }
-        if ($user->role === 'guru'){
-            $data_user = User::with('teacher')->where('id_number', $input['id_number'])->where('password', $input['password'])->first();
-        }
-
         return response([
-            'data' => $data_user
+            'data' => $user
         ]);
     }
 }
